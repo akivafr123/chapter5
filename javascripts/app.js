7b12f3f82243517ee794d58ac1e87f8b2b8d98d7
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-04-01 08:09:02 -0400

    Added flickr images using JSON feed

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,24 @@
 var main = function() {
-	"use strict";
+	//"use strict";
 
 	// this is actually just one string,
 	// but I spread it out over two lines
 	// to make it more readable
-	var url = "http://api.flickr.com/services/feeds/photos_public.gne?%22%20+%20%22tags=dogs&format=json"
+	var url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?"
 
 	$.getJSON(url, function(flickrResponse) {
-		// we'll simply print the response
-		// to the console for the time being
-		console.log(flickrResponse);
+		flickrResponse.items.forEach(function(photo) {
+			// Create a new JQuery element to hold the image
+			// hide it so we can fade it in
+			var $img = $("<img>").hide();
+
+			// set the attribute to the url contained in the response
+			$img.attr("src", photo.media.m);
+
+			// attach the img tag to the main photos element
+			$("main .photos").append($img);
+			$img.fadeIn();
+		});
 	});
 };
 
