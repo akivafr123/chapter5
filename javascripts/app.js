c5583a7cfa4df6d706397a471b847d233c2d65d3
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-04-01 06:21:18 -0400

    Initial commit

---
+++ javascripts/app.js
@@ -0,0 +1,16 @@
+var main = function() {
+	"use strict";
+
+	// this is actually just one string,
+	// but I spread it out over two lines
+	// to make it more readable
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?%22%20+%20%22tags=dogs&format=json"
+
+	$.getJSON(url, function(flickrResponse) {
+		// we'll simply print the response
+		// to the console for the time being
+		console.log(flickrResponse);
+	});
+};
+
+$(document).ready(main);
